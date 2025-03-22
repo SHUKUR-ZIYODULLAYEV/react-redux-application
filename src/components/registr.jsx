49e3498da7b597b4476f88f161b517a0b165699d
SHUKUR <ziyodullayevshukur1998@gmail.com>
2025-03-22 18:16:47 +0500

    #9. Login & Register page & UI

--- src/components/registr.jsx
+++ src/components/registr.jsx
@@ -1,24 +1,25 @@
 import React, { useState } from 'react'
-import { logo } from '../constants'
 import { Input } from '../ui'
+import { logo } from '../constants'
 
-function Login() {
+function Registr() { 
   const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   return (
     <div className='text-center mt-5' >
       <main className="form-signin w-25 m-auto">
         <form>
           <img className="mb-4" src={logo} alt="" width="72" height="57"/>
-          <h1 className="h3 mb-3 fw-normal">Please login</h1>
+          <h1 className="h3 mb-3 fw-normal">Please registr</h1>
           <Input setState={setName} state={name} type='text' id='floatingInput' placeholder='Username' label='Username' />
+          <Input setState={setEmail} state={email} type='email' id='floatingInput' placeholder='name@example.com' label='Email address' />
           <Input setState={setPassword} state={password} type='password' id='floatingPassword' placeholder='Password' label='Password' />
-          <button className="btn btn-lg btn-primary w-100 mt-2" type="submit">Login</button>
+          <button className="btn btn-lg btn-primary w-100 mt-2" type="submit">Registr</button>
         </form>
       </main>
     </div>
-    
   )
 }
 
-export default Login
+export default Registr
---
+++ src/ui/input.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Input({type, id, placeholder, label, state, setState}) {
+  return (
+    <div className="form-floating">
+        <input type={type} value={state} onChange={e => setState(e.target.value)} className="form-control" id={id} placeholder={placeholder}/>
+        <label for={id}>{label}</label>
+    </div>
+  )
+}
+
+export default Input
